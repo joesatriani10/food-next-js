92aecbf93981d2a45f0407e1c3ce898f080ae0ed
Angel Montes de Oca <joesatriani10@gmail.com>
2024-03-29 17:18:33 -0700

    Fixed share meal button behavior

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,9 +1,13 @@
 'use client';
-import {useFormStatus} from "react-dom";
 
-const MealsFormSubmit = () => {
-    const {pending} = useFormStatus;
-    return <button disabled={pending}>{pending ? 'Submitting...' : 'Share Meal'}</button>
+import{ useFormStatus } from 'react-dom';
+
+export default function MealsFormSubmit() {
+    const { pending } = useFormStatus();
+
+    return (<button disabled={pending}>
+        {pending ? 'Submitting...' : 'Share Meal'}
+    </button>);
+
+
 }
-
-export default MealsFormSubmit;
