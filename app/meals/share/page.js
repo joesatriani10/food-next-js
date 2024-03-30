99113368f0761e3dc894f9ee683e36aeb7708d7b
Angel Montes de Oca <joesatriani10@gmail.com>
2024-03-29 17:48:57 -0700

    Added backend form validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,5 @@
+'use client';
+import {useFormState} from 'react-dom';
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
@@ -5,50 +7,52 @@ import {shareMeal} from "@/lib/actions";
 
 
 export default function ShareMealPage() {
+    const [state, formAction] = useFormState(shareMeal, {message: null});
 
-return (
-    <>
-        <header className={classes.header}>
-            <h1>
-                Share your <span className={classes.highlight}>favorite meal</span>
-            </h1>
-            <p>Or any other meal you feel needs sharing!</p>
-        </header>
-        <main className={classes.main}>
-            <form className={classes.form} action={shareMeal}>
-                <div className={classes.row}>
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Share your <span className={classes.highlight}>favorite meal</span>
+                </h1>
+                <p>Or any other meal you feel needs sharing!</p>
+            </header>
+            <main className={classes.main}>
+                <form className={classes.form} action={formAction}>
+                    <div className={classes.row}>
+                        <p>
+                            <label htmlFor="name">Your name</label>
+                            <input type="text" id="name" name="name" required/>
+                        </p>
+                        <p>
+                            <label htmlFor="email">Your email</label>
+                            <input type="email" id="email" name="email" required/>
+                        </p>
+                    </div>
                     <p>
-                        <label htmlFor="name">Your name</label>
-                        <input type="text" id="name" name="name" required/>
+                        <label htmlFor="title">Title</label>
+                        <input type="text" id="title" name="title" required/>
                     </p>
                     <p>
-                        <label htmlFor="email">Your email</label>
-                        <input type="email" id="email" name="email" required/>
+                        <label htmlFor="summary">Short Summary</label>
+                        <input type="text" id="summary" name="summary" required/>
                     </p>
-                </div>
-                <p>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" required/>
-                </p>
-                <p>
-                    <label htmlFor="summary">Short Summary</label>
-                    <input type="text" id="summary" name="summary" required/>
-                </p>
-                <p>
-                    <label htmlFor="instructions">Instructions</label>
-                    <textarea
-                        id="instructions"
-                        name="instructions"
-                        rows="10"
-                        required
-                    ></textarea>
-                </p>
-                <ImagePicker label="Your image" name="image"/>
-                <p className={classes.actions}>
-                    <MealsFormSubmit/>
-                </p>
-            </form>
-        </main>
-    </>
-);
+                    <p>
+                        <label htmlFor="instructions">Instructions</label>
+                        <textarea
+                            id="instructions"
+                            name="instructions"
+                            rows="10"
+                            required
+                        ></textarea>
+                    </p>
+                    <ImagePicker label="Your image" name="image"/>
+                    {state.message && <p>{state.message}</p>}
+                    <p className={classes.actions}>
+                        <MealsFormSubmit/>
+                    </p>
+                </form>
+            </main>
+        </>
+    );
 }
