bc2eaac33e39bbf9e6368831e9f61fd049de9ef2
Angel Montes de Oca <joesatriani10@gmail.com>
2024-04-07 19:22:48 -0700

    allows S3 URL as a valid source for images.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'angelnextjsdemo.s3.us-west-1.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 module.exports = nextConfig
