9870207b81868cfe99b8bb9e5d8b6e04bdaa7e50
joesatriani10 <joesatriani10@gmail.com>
2024-04-11 14:55:17 -0700

    Updated metadata slug

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -3,6 +3,19 @@ import Image from "next/image";
 import {getMeal} from "@/lib/meals"
 import {notFound} from "next/navigation";
 
+export async function generateMetadata({params}) {
+    const meal = await getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+
+    return {
+        title: meal.title,
+        description: meal.summary,
+    }
+}
+
 const MealPost = async ({params}) => {
     const meal = await getMeal(params.mealSlug);
 
